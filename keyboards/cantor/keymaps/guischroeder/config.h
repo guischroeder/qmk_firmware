c2a394fa4ed5fe556be94ede00bb06a1f793f52f
guischroeder <guilherme.schroederr@gmail.com>
2025-03-27 00:21:21 -0300

    Add cantor keyboard keymap

---
+++ keyboards/cantor/keymaps/guischroeder/config.h
@@ -0,0 +1,23 @@
+#pragma once
+
+/* Select hand configuration */
+#define MASTER_LEFT
+#define USE_SERIAL_PD2
+
+// Tapping configuration
+#define TAPPING_TERM 500
+#define TAPPING_TERM_PER_KEY
+#define TAPPING_TERM_THUMB 500
+#define HOLD_ON_OTHER_KEY_PRESS
+#define PREVENT_STUCK_MODIFERS
+
+// Combo settings
+#define COMBO_COUNT 1
+#define COMBO_TERM 40
+
+// One-shot modifier settings
+#define ONESHOT_TAP_TOGGLE 3
+#define ONESHOT_TIMEOUT 2000
+
+// Advanced key behaviors
+#define PERMISSIVE_HOLD
---
+++ keyboards/cantor/keymaps/guischroeder/rules.mk
@@ -0,0 +1,12 @@
+# Basic features
+EXTRAKEY_ENABLE = yes
+LINK_TIME_OPTIMIZATION_ENABLE = yes
+COMBO_ENABLE = yes
+
+# Lighting and advanced features
+BACKLIGHT_ENABLE = no
+RGBLIGHT_ENABLE = no
+TAP_DANCE_ENABLE = no
+
+# Optional: Uncomment if needed
+# BOOTMAGIC_ENABLE = lite
